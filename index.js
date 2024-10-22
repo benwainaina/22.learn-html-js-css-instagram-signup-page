a64e040c19291dab67073a771e3ddffec94ecab1
ben wainaina <bwainainamwangi@gmail.com>
2024-10-22 20:42:12 +0300

    finalize on header

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Uses:
+ *
+ * 1. Validation of valid signup form.
+ * 2. Cross-check with the server if the entered email and username are
+ * available for use
+ * 3. Handling click events
+ */
